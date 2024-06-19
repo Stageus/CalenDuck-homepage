6e9e82fe8a28c43e6987c5d53686b100e544e418
Kyoungeun Jo <kavvy99@naver.com>
2024-06-19 14:22:39 +0900

    feat: create searchItem component

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import HeaderItem from "shared/components/HeaderItem";
+import SearchItem from "widgets/SearchItem";
 
 const MainPage = () => {
   return (
     <>
       <HeaderItem />
       <h1>메인페이지</h1>
+      <SearchItem />
     </>
   );
 };
