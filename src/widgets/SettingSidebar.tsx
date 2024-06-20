db28a11590080937ee2c18587447c4db0d34899f
Kyoungeun Jo <kavvy99@naver.com>
2024-06-20 15:09:46 +0900

    feat: create MySubjectItem component

--- src/widgets/SettingSidebar.tsx
+++ src/widgets/SettingSidebar.tsx
@@ -1,9 +1,32 @@
-import React, { useState, useRef } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 
 import search from "shared/imgs/search.svg";
+import MySubjectItem from "widgets/MySubjectItem";
 
 const SettingSidebar = () => {
+  const dummyData = [
+    {
+      id: "mySubject_1",
+      subject: "미식축구",
+    },
+    {
+      id: "mySubject_2",
+      subject: "야구",
+    },
+    {
+      id: "mySubject_3",
+      subject: "분데스리가",
+    },
+    {
+      id: "mySubject_4",
+      subject: "NBA",
+    },
+    {
+      id: "mySubject_5",
+      subject: "뮤지컬",
+    },
+  ];
   return (
     <section className="w-[310px] h-sidebar bg-sidebarColor flex flex-col justify-start items-center p-[20px]">
       {/* 검색기능 */}
@@ -11,7 +34,6 @@ const SettingSidebar = () => {
         <div className="relative">
           <input
             type="text"
-            required
             placeholder="관심사를 입력하세요"
             className="w-[100%] h-[42px] p-[5px] border border-black rounded-[5px] focus:border-none focus:outline-none focus:shadow focus:shadow-inputFocus"
           />
@@ -32,12 +54,16 @@ const SettingSidebar = () => {
           <h3 className="mr-[5px] font-semibold">내 관심사</h3>
           <span className="text-alertColor text-xs">최대 5개까지 선택 가능합니다</span>
         </div>
-        <div className="border border-blue-500 w-[100%] h-[187px] mt-[10px]">관심사 목록 5개</div>
+        <div className="w-[100%] h-[250px] mt-[10px] flex flex-col justify-start">
+          {dummyData.map((elem) => {
+            return <MySubjectItem key={elem.id} data={elem} />;
+          })}
+        </div>
       </article>
 
       {/* 하단 기능 버튼 */}
       <article className="w-[100%] h-[130px] mt-auto flex flex-col justify-between items-end">
-        <Link to="/manage?category=미식축구">
+        <Link to="/manage?subject=미식축구">
           <button className="text-sm px-[10px] py-[5px] rounded-[5px] hover:bg-subColor">
             관심사 관리 페이지로 이동
           </button>
