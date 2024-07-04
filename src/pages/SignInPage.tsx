6180958a39daeba00a623742999f11d1ed37e124
Kyoungeun Jo <kavvy99@naver.com>
2024-07-04 23:09:42 +0900

    feat: create SetNewPwPage

--- src/pages/SignInPage.tsx
+++ src/pages/SignInPage.tsx
@@ -15,12 +15,8 @@ const SignInPage = () => {
       <article className="flex flex-col justify-center items-center w-[55vw] bg-white rounded-l-[30px]">
         <div className="h-[40%] flex flex-col justify-around">
           <div className="w-[100%]">
-            <InputItem label="아이디" type="text" placeholder="최대 10자 이내로 입력해주세요" />
-            <InputItem
-              label="비밀번호"
-              type="password"
-              placeholder="최대 20자 이내로 입력해주세요"
-            />
+            <InputItem label="아이디" type="text" placeholder="" />
+            <InputItem label="비밀번호" type="password" placeholder="" />
           </div>
 
           <div className="flex flex-col w-[100%]">
@@ -34,16 +30,16 @@ const SignInPage = () => {
 
           <div className="flex justify-between">
             <Link to="/signUp">
-              <span>계정이 없으세요? 회원가입</span>
+              <span className="text-sm">계정이 없으세요? 회원가입</span>
             </Link>
 
             <div className="flex">
               <Link to="/findId">
-                <span>아이디 찾기 &nbsp;</span>
+                <span className="text-sm">아이디 찾기 &nbsp;</span>
               </Link>
               /
               <Link to="/findPw">
-                <span> &nbsp; 비밀번호 찾기</span>
+                <span className="text-sm"> &nbsp; 비밀번호 찾기</span>
               </Link>
             </div>
           </div>
