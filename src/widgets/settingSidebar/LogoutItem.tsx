90f4336c525f4344b247d27e6c70210e66142e22
Kyoungeun Jo <kavvy99@naver.com>
2024-08-13 15:33:04 +0900

    feat: remove token to logout

--- src/widgets/settingSidebar/LogoutItem.tsx
+++ src/widgets/settingSidebar/LogoutItem.tsx
@@ -1,9 +1,24 @@
 import React from "react";
+import { useCookies } from "react-cookie";
+import { useResetRecoilState } from "recoil";
+import userInfoAtom from "shared/recoil/userInfoAtom";
 
-// 로그아웃 api 연결
+// 로그아웃
 const LogoutItem = () => {
+  const [, , removeCookie] = useCookies(["token"]);
+  const resetUserInfo = useResetRecoilState(userInfoAtom);
+
+  const logoutEvent = () => {
+    removeCookie("token", { path: "/" });
+    resetUserInfo();
+  };
   return (
-    <button className="text-sm px-[10px] py-[5px] rounded-[5px] hover:bg-subColor">로그아웃</button>
+    <button
+      onClick={logoutEvent}
+      className="text-sm px-[10px] py-[5px] rounded-[5px] hover:bg-subColor"
+    >
+      로그아웃
+    </button>
   );
 };
 
