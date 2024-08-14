3bbf640019edb29570747d73cc989caf1003e065
Kyoungeun Jo <kavvy99@naver.com>
2024-08-14 16:26:01 +0900

    feat: connect DELETE api for deleteSchedule

--- src/widgets/scheduleModal/ScheduleItem.tsx
+++ src/widgets/scheduleModal/ScheduleItem.tsx
@@ -6,9 +6,11 @@ import edit from "shared/imgs/edit.svg";
 import remove from "shared/imgs/remove.svg";
 import finish from "shared/imgs/finish.svg";
 import { TScheduleItem } from "types";
+import { useCookies } from "react-cookie";
 
 const ScheduleItem: React.FC<{ data: TScheduleItem }> = (props) => {
   const { idx, name, time, type, contents, priority } = props.data;
+  const [cookies] = useCookies(["token"]);
 
   // 스케줄 알람 여부 버튼 토글
   const [alarm, setAlarm] = useState<boolean>(priority);
@@ -27,6 +29,29 @@ const ScheduleItem: React.FC<{ data: TScheduleItem }> = (props) => {
     setEditing(!editing);
     if (!editing && titleRef.current) {
       titleRef.current.value = contents;
+    }
+  };
+
+  // 스케줄 삭제 DELETE api (/schedules/:idx)
+  const deleteScheduleEvent = async () => {
+    try {
+      const response = await fetch(`${process.env.REACT_APP_API_KEY}/schedules/${idx}`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${cookies.token}`,
+        },
+      });
+
+      if (response.ok) {
+        alert(`해당 스케줄을 삭제했습니다.`);
+      } else if (response.status === 401) {
+        console.log("잘못된 인증 정보 제공");
+        alert(`스케줄 삭제에 실패했습니다.`);
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      alert(`스케줄 삭제 중 오류가 발생했습니다.`);
     }
   };
 
@@ -76,7 +101,7 @@ const ScheduleItem: React.FC<{ data: TScheduleItem }> = (props) => {
               <button onClick={editTitleEvent}>
                 <img src={edit} alt="수정하기" />
               </button>
-              <button>
+              <button onClick={deleteScheduleEvent}>
                 <img src={remove} alt="삭제하기" />
               </button>
             </>
