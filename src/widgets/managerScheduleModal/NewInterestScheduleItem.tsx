8cc58528baf30e092b3ed6bd4c5850b4a8ffdd4a
Kyoungeun Jo <kavvy99@naver.com>
2024-08-15 18:33:17 +0900

    fix: change fullDate value for post newInterestSchedule

--- src/widgets/managerScheduleModal/NewInterestScheduleItem.tsx
+++ src/widgets/managerScheduleModal/NewInterestScheduleItem.tsx
@@ -8,9 +8,18 @@ import selectedDateAtom from "shared/recoil/selectedDateAtom";
 // 관심사 스케줄 생성 POST api 연결 (/managers/schedules/interests)
 const NewInterestScheduleItem = () => {
   const [cookies] = useCookies(["token"]);
+  const [scheduleTime, setScheduleTime] = useState("");
   const [interestContents, setInterestContents] = useState("");
-  const [selectedDate] = useRecoilState(selectedDateAtom);
 
+  const [selectedDate] = useRecoilState(selectedDateAtom);
+  const year = selectedDate && selectedDate.getFullYear();
+  const month = selectedDate && (selectedDate.getMonth() + 1).toString().padStart(2, "0");
+  const date = selectedDate && selectedDate.getDate().toString().padStart(2, "0");
+
+  const selectedTime = scheduleTime.split(":").join("");
+  const fullDate = `${year}${month}${date}${selectedTime}`;
+
+  console.log(fullDate);
   const submitNewInterestSchedule = async () => {
     try {
       const response = await fetch(
@@ -22,7 +31,7 @@ const NewInterestScheduleItem = () => {
             Authorization: `Bearer ${cookies.token}`,
           },
           body: JSON.stringify({
-            fullDate: selectedDate,
+            fullDate: fullDate,
             interestContents: interestContents,
           }),
         }
@@ -50,7 +59,7 @@ const NewInterestScheduleItem = () => {
     <article className="w-[655px] h-[15%] p-[20px] flex justify-between border-y border-black">
       <div className="w-[85%] flex items-center">
         <div>
-          <input type="time" />
+          <input type="time" onChange={(e) => setScheduleTime(e.target.value)} />
         </div>
         <input
           type="text"
