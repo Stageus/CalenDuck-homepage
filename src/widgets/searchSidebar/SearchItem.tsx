42a0d305f77494ff27a86bedc36100562f02242d
Kyoungeun Jo <kavvy99@naver.com>
2024-08-18 21:46:31 +0900

    feat: connect GET api for search schedule results

--- src/widgets/searchSidebar/SearchItem.tsx
+++ src/widgets/searchSidebar/SearchItem.tsx
@@ -1,25 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { TScheduleItem } from "types";
 
-interface SearchItemProps {
-  data: {
-    title: string;
-    date: string;
-  };
-}
-
-const SearchItem: React.FC<SearchItemProps> = (props) => {
-  const { title, date } = props.data;
+const SearchItem: React.FC<{ data: TScheduleItem }> = (props) => {
+  const { idx, time, contents } = props.data;
 
   return (
     // 클릭 시 해당 날짜에 맞는 모달창 출력
-    <Link to="/">
-      <button
-        className="w-full h-[42px] flex justify-between items-center p-[10px] mb-[10px] bg-lightgrayColor rounded-[5px]"
-        title={title}
-      >
-        <div className="w-[65%] font-normal text-sm truncate mr-[20px]">{title}</div>
-        <div className="w-[35%] font-normal text-sm">{date}</div>
+    <Link to="">
+      <button className="w-full h-[42px] flex justify-between items-center p-[10px] mb-[10px] bg-lightgrayColor rounded-[5px]">
+        <div className="w-[65%] font-normal text-sm truncate mr-[20px]">{contents}</div>
+        <div className="w-[35%] font-normal text-sm">{time}</div>
       </button>
     </Link>
   );
--- src/widgets/searchSidebar/SearchSidebar.tsx
+++ src/widgets/searchSidebar/SearchSidebar.tsx
@@ -7,72 +7,76 @@ import SearchItem from "widgets/searchSidebar/SearchItem";
 import SearchDateUtil from "widgets/searchSidebar/SearchDateUtil";
 
 import search from "shared/imgs/search.svg";
+import { useCookies } from "react-cookie";
+import { TScheduleItem } from "types";
 
+// 스케줄 검색 결과 불러오기 GET api 연결 (/schedules/searches?start_date&end_date&content)
 const SearchSidebar: React.FC = () => {
-  const dummyData = [
-    {
-      id: "searchResult_1",
-      title: "알고리즘 11주차 과제 마감akrkakdalkjdflk",
-      date: "2024/06/04",
-    },
-    {
-      id: "searchResult_2",
-      title: "알고리즘 10주차 과제 마감",
-      date: "2024/05/10",
-    },
-    {
-      id: "searchResult_3",
-      title: "알고리즘 9주차 과제 마감",
-      date: "2024/05/03",
-    },
-    {
-      id: "searchResult_4",
-      title: "알고리즘 8주차 과제 마감",
-      date: "2024/04/26",
-    },
-    {
-      id: "searchResult_5",
-      title: "알고리즘 7주차 과제 마감",
-      date: "2024/04/19",
-    },
-    {
-      id: "searchResult_6",
-      title: "알고리즘 7주차 과제 마감",
-      date: "2024/04/19",
-    },
-    {
-      id: "searchResult_7",
-      title: "알고리즘 7주차 과제 마감",
-      date: "2024/04/19",
-    },
-    {
-      id: "searchResult_8",
-      title: "알고리즘 7주차 과제 마감",
-      date: "2024/04/19",
-    },
-    {
-      id: "searchResult_9",
-      title: "알고리즘 7주차 과제 마감",
-      date: "2024/04/19",
-    },
-    {
-      id: "searchResult_10",
-      title: "알고리즘 7주차 과제 마감",
-      date: "2024/04/19",
-    },
-    {
-      id: "searchResult_11",
-      title: "알고리즘 7주차 과제 마감",
-      date: "2024/04/19",
-    },
-    {
-      id: "searchResult_12",
-      title: "알고리즘 7주차 과제 마감",
-      date: "2024/04/19",
-    },
-  ];
+  // const dummyData = [
+  //   {
+  //     id: "searchResult_1",
+  //     title: "알고리즘 11주차 과제 마감akrkakdalkjdflk",
+  //     date: "2024/06/04",
+  //   },
+  //   {
+  //     id: "searchResult_2",
+  //     title: "알고리즘 10주차 과제 마감",
+  //     date: "2024/05/10",
+  //   },
+  //   {
+  //     id: "searchResult_3",
+  //     title: "알고리즘 9주차 과제 마감",
+  //     date: "2024/05/03",
+  //   },
+  //   {
+  //     id: "searchResult_4",
+  //     title: "알고리즘 8주차 과제 마감",
+  //     date: "2024/04/26",
+  //   },
+  //   {
+  //     id: "searchResult_5",
+  //     title: "알고리즘 7주차 과제 마감",
+  //     date: "2024/04/19",
+  //   },
+  //   {
+  //     id: "searchResult_6",
+  //     title: "알고리즘 7주차 과제 마감",
+  //     date: "2024/04/19",
+  //   },
+  //   {
+  //     id: "searchResult_7",
+  //     title: "알고리즘 7주차 과제 마감",
+  //     date: "2024/04/19",
+  //   },
+  //   {
+  //     id: "searchResult_8",
+  //     title: "알고리즘 7주차 과제 마감",
+  //     date: "2024/04/19",
+  //   },
+  //   {
+  //     id: "searchResult_9",
+  //     title: "알고리즘 7주차 과제 마감",
+  //     date: "2024/04/19",
+  //   },
+  //   {
+  //     id: "searchResult_10",
+  //     title: "알고리즘 7주차 과제 마감",
+  //     date: "2024/04/19",
+  //   },
+  //   {
+  //     id: "searchResult_11",
+  //     title: "알고리즘 7주차 과제 마감",
+  //     date: "2024/04/19",
+  //   },
+  //   {
+  //     id: "searchResult_12",
+  //     title: "알고리즘 7주차 과제 마감",
+  //     date: "2024/04/19",
+  //   },
+  // ];
 
   // 검색 기간의 시작점 지정에 따른 끝점의 최소값 설정
+
   const startDateRef = useRef<HTMLInputElement>(null);
   const endDateRef = useRef<HTMLInputElement>(null);
   const keyWordRef = useRef<HTMLInputElement>(null);
@@ -83,16 +87,42 @@ const SearchSidebar: React.FC = () => {
 
   // 검색기간, 검색어 중 하나라도 미입력 후 검색 진행 시 경고문구 출력
   const [alert, setAlert] = useState<boolean>(false);
+  const [cookies] = useCookies(["token"]);
+  const [seachScheduleList, setSearchScheduleList] = useState<TScheduleItem[]>([]);
 
-  const clickSearchEvent = () => {
-    const startDateValue = startDateRef.current?.value;
-    const endDateValue = endDateRef.current?.value;
+  const clickSearchEvent = async () => {
+    const startDateValue = startDateRef.current?.value.split("-").join("");
+    const endDateValue = endDateRef.current?.value.split("-").join("");
     const keyWordValue = keyWordRef.current?.value;
 
     if (!startDateValue || !endDateValue || !keyWordValue) {
       setAlert(true);
     } else {
       setAlert(false);
+      // 빈칸이 아니라면 데이터 GET
+      try {
+        const response = await fetch(
+          `${process.env.REACT_APP_API_KEY}/schedules/searches?start_date=${startDateValue}&end_date=${endDateValue}&content=${keyWordValue}`,
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${cookies.token}`,
+            },
+          }
+        );
+        const result = await response.json();
+
+        if (response.status === 200) {
+          setSearchScheduleList(result.list);
+        } else if (response.status === 400) {
+          console.log("정규식 위반");
+        } else if (response.status === 401) {
+          console.log("잘못된 인증 정보 제공");
+        }
+      } catch (error) {
+        console.error("서버 에러: ", error);
+      }
     }
   };
 
@@ -115,14 +145,14 @@ const SearchSidebar: React.FC = () => {
             <input
               type="date"
               required
-              className="pl-1 h-[42px] text-sm	border border-black rounded-[5px] focus:border-none focus:outline-none focus:shadow focus:shadow-inputFocus"
+              className="p-1 h-[42px] text-sm	border border-black rounded-[5px] focus:border-none focus:outline-none focus:shadow focus:shadow-inputFocus"
               ref={startDateRef}
             />
             <span>-</span>
             <input
               type="date"
               required
-              className="pl-1 h-[42px] text-sm border border-black rounded-[5px] focus:border-none focus:outline-none focus:shadow focus:shadow-inputFocus"
+              className="p-1 h-[42px] text-sm border border-black rounded-[5px] focus:border-none focus:outline-none focus:shadow focus:shadow-inputFocus"
               ref={endDateRef}
             />
           </div>
@@ -153,8 +183,8 @@ const SearchSidebar: React.FC = () => {
 
       {/* 검색결과 리스트 */}
       <article className="w-full h-[80%] overflow-auto">
-        {dummyData.map((elem) => {
-          return <SearchItem key={elem.id} data={elem} />;
+        {seachScheduleList.map((elem) => {
+          return <SearchItem key={elem.idx} data={elem} />;
         })}
       </article>
     </section>
